e98fb6fbf6408c8f4331c46a68ce21b5b0030ab3
Amr AbulAzm <amrabulazm@amrs-mbp>
2016-12-01 11:29:02 +0100

    Initial Commit

---
+++ NYTimes/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  NYTimes
+//
+//  Created by Amr AbulAzm on 01/12/2016.
+//  Copyright © 2016 Amr AbulAzm. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ NYTimes/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  NYTimes
+//
+//  Created by Amr AbulAzm on 01/12/2016.
+//  Copyright © 2016 Amr AbulAzm. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
